6ddd79b635a9b4eef5b53771b774f54b7dfde36e
hamzazaouya <hamzazaouya18@gmail.com>
2021-06-11 16:29:08 +0100

    final edite

--- algo.c
+++ algo.c
@@ -1,64 +1,25 @@
-#include <stdio.h>
-
-int power(int a,int b) {
-
-	int i = 1;
-	while(i < b) {
-
-		a = a * a;
-		i++;
-	}
-
-	return a;
-
-}
-
-float squreRoot(int a ){
-
-	int number = a;
-	float temp, sqrt;
-	sqrt = number / 2;
-	temp = 0;
-	
-	while(sqrt != temp){
-
-		temp = sqrt;
-		sqrt = (number/temp + temp)/2;
-	
-	}
-
-}
-
+#include<stdio.h>
  
 int main(){
-	
-	float	a = 25;
-	float	b = 35;
-	float	c = 12;
-	float	dt = 0;
-	float	r1 = 0;
-	float	r2 = 0;
-	
-	dt = power(b,2) -(4*a*c);
 
-	if(dt > 0){
+
+        char  month[12][3] = {"Jan", "Feb", "Mar","Apr", "Mai", "Jun", "jul", "Aug", "Sep", "Oct", "Nov", "Dec"}; 
+
+        int n = 0;
+	int i = 0;
+        printf("Enter a Number");
+	scanf("%d", &n);
+
+        for( i = 0; i < 3; i++){
 		
-		r1 = (-b + squreRoot(dt))/(2*a);
-		r2 = (-b - squreRoot(dt))/(2*a);
+		printf("%c", month[n-1][i]);		
 
-		printf("Root1 = %f\n", r1);
-		printf("Root2 = %f\n", r2);
-
-	}else if(dt = 0){
-		
-		r1 = -b/(2*a);
-		printf("Root = %.2f", r1);
-			
-	}else {
-
-		printf("This Equation Dont have a Solution in R");
-		
 	}
-	
-	return 0;
+
+	printf("\n");
+
+        return 0;
+
+
+
 }
